883cd4fbef01c64dd748e1eb4dabf8b55126d14a
tzebp <tzebp@proton.me>
2025-06-09 09:58:18 -0400

    hapus bantuan tambahan

--- js/book-names.js
+++ js/book-names.js
@@ -1,8 +1,5 @@
 var books = [
   { name: "Asal Mulo Dunio", ref: "Aktb-01-GEN-001.html" },
   { name: "Zabór", ref: "Aktb-02-PSA-001.html" },
-  { name: "Penjelasan Penggunaan Bantuan", ref: "Aktb-03-PPB-001.html" },
-  { name: "Daptar Singkatan Namo Kitab", ref: "Aktb-04-FRT-2-001.html" },
-  { name: "Kamus Kato Kunci", ref: "Aktb-05-GLO-000.html" },
-  { name: "Pertanyoan Belajar Alkitab", ref: "Aktb-06-DBS-001.html" },
+  { name: "GLO", ref: "Aktb-03-GLO-001.html" },
 ];
