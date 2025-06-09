1cdbf70242658f8828d3e739358e8275f04e1b7f
tzebp <tzebp@proton.me>
2025-06-09 10:16:10 -0400

    hapus file GLO

--- js/book-names.js
+++ js/book-names.js
@@ -1,5 +1,4 @@
 var books = [
   { name: "Asal Mulo Dunio", ref: "Aktb-01-GEN-001.html" },
-  { name: "Zabór", ref: "Aktb-02-PSA-001.html" },
-  { name: "GLO", ref: "Aktb-03-GLO-001.html" },
+  { name: "Zabór", ref: "Aktb-02-PSA-001.html" }
 ];
